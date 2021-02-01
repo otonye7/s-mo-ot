73539a5590a8347a108577475214eaa099f5775d
otonye <otonyeamie@gmail.com>
2021-02-01 16:25:57 +0100

    master

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom'; 
+import './App.css';
 
 function App() {
   return (
